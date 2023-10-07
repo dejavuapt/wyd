b13b9cb1319675fea2599968112bcf0e9d0683bf
dejavuapt <thepoli.b@gmail.com>
2023-10-07 23:27:51 +0500

    add stop_callback def

--- app/main.py
+++ app/main.py
@@ -18,15 +18,17 @@ async def callback_answer(context: ContextTypes.DEFAULT_TYPE):
     # Beep the person who called this alarm:
     await context.bot.send_message(
         chat_id=context.job.chat_id, 
-        text=f'Что делаешь，{context.job.data}?'
+        text=f'[NOTIFICATION] What are you doing, {context.job.data}? '
         )
     
+
+# напоминалка с утра до вечера каждый час 
 async def callback_in_day(context: ContextTypes.DEFAULT_TYPE):
     context.job_queue.run_repeating(
         callback_answer, 
-        interval = 10, 
-        first = t.fromisoformat("08:00:00+05:00"),
-        last = t.fromisoformat("23:59:00+05:00"),
+        interval = constants.INTERVAL_REMINDER, 
+        first = t.fromisoformat("07:00:00+05:00"),
+        last = t.fromisoformat("22:00:00+05:00"),
         data = context.job.data['name'], 
         chat_id = context.job.data['chat_id'])
 
@@ -37,17 +39,24 @@ async def callback_daily(update: Update, context: ContextTypes.DEFAULT_TYPE):
     name = update.effective_chat.full_name
     await context.bot.send_message(
         chat_id = chat_id, 
-        text = '[INFO] starting interval job...'
+        text = '[INFO] Starting interval job...'
         )
     # Set the alarm:
     context.job_queue.run_daily(
         callback_in_day,
-        time = t.fromisoformat("22:54:00+05:00"),
+        time = t.fromisoformat("06:00:00+05:00"),
         data = {'name': name, 'chat_id': chat_id},
         chat_id = chat_id,
     )
-    print(context.job_queue.jobs())
 
+
+async def stop_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if(len(context.job_queue.jobs()) > 1):
+        context.job_queue.jobs()[0].schedule_removal() # удаляем тот что дневной если он включен, ажедневный оставляем
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = '[INFO] Stop callback. Next tomorrow!'
+    )
 
 #команда деланья
 async def doing(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -55,7 +64,7 @@ async def doing(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(text_doing)
     await context.bot.send_message(
         chat_id = update.effective_chat.id,
-        text = '[INFO] Result: 0'
+        text = '[INFO] Writed!'
     )
 
 
@@ -65,6 +74,9 @@ if __name__ == '__main__':
 
     doing_handler = CommandHandler('doing', doing)
     application.add_handler(doing_handler)
+
+    stop_callback_handler = CommandHandler('stop_callback', stop_callback)
+    application.add_handler(stop_callback_handler)
 
     timer_handler = CommandHandler('oneday', callback_daily)
     application.add_handler(timer_handler)
