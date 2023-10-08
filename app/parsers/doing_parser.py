66a42e9a07178baddc159cf2821e14d17a1309cc
dejavuapt <thepoli.b@gmail.com>
2023-10-08 23:22:29 +0500

    add AddDateRow

--- app/parsers/doing_parser.py
+++ app/parsers/doing_parser.py
@@ -1,6 +1,7 @@
 
 import csv;
 import os;
+from datetime import date;
 
 class DoingParser:
 
@@ -46,3 +47,8 @@ class DoingParser:
     def RefreshCSV(self) -> None:
         with open(self.csv_path, 'w') as file:
             file.write(self.header + '\n')
+
+
+    def AddDateRow(self, current_dt: date) -> None:
+        with open(self.csv_path, 'a',encoding='UTF8') as file:
+            file.write(f'{current_dt.strftime("%d.%m.%y")}{self.sep}{self.sep}{self.sep}')
