064799e258ad97439fc3b91a80f3957383f5ec65
dejavuapt <thepoli.b@gmail.com>
2023-10-08 23:13:54 +0500

    add setSeparate

--- app/parsers/doing_parser.py
+++ app/parsers/doing_parser.py
@@ -4,10 +4,14 @@ import os;
 
 class DoingParser:
 
-    def __init__(self, doing_string: str, sign: str = '|') -> None:
-        self.separate_string: list = doing_string.split(sign)
-        print(self.separate_string)
+    def __init__(self, csv_path: str, header: str = "time;doing_text;flag;hours") -> None:
+        self.separate_string: list = []
         self.sep:str = ';'
+        self.csv_path = csv_path
+        self.header_csv = header
+
+    def SetSeparate(self, doing_string: str, sign: str = '|') -> None:
+        self.separate_string = doing_string.split(sign);
 
     def ParseTime(self) -> str:
         return self.separate_string[0]
@@ -33,12 +37,12 @@ class DoingParser:
     def CreateFlagsStroke(self) -> list:
         return [f'{self.sep}{flag}\n' for flag in self.ParseFlags()]
     
-    def WriteInCSV(self, path_of_csv: str) -> None:
-        with open(path_of_csv, 'a',encoding='UTF8') as file:
+    def WriteInCSV(self) -> None:
+        with open(self.csv_path, 'a',encoding='UTF8') as file:
             file.write(self.CreateTimeDoingStroke())
             file.writelines(self.CreateFlagsStroke())
 
 
-    def RefreshCSV(self, path_of_csv: str, header:str) -> None:
-        with open(path_of_csv, 'w') as file:
-            file.write(header + '\n')
+    def RefreshCSV(self) -> None:
+        with open(self.csv_path, 'w') as file:
+            file.write(self.header + '\n')
